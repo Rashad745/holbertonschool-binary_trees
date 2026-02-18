713af0f5a35f96b81460a8d9c2007540b2b2ebbd
Rashad745 <rnuraliyev@gmail.com>
2026-02-18 12:28:55 +0000

    filetreeleaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -17,5 +17,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	/* Sum leaves of left and right subtrees */
 	return (binary_tree_leaves(tree->left) +
-	        binary_tree_leaves(tree->right));
+		binary_tree_leaves(tree->right));
 }
+
