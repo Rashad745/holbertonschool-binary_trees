b619d0cd2690f446e9a822bb6e2d3ee6147392fc
Rashad745 <rnuraliyev@gmail.com>
2026-02-18 12:26:35 +0000

    fuketreeleaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: pointer to the root node
+ *
+ * Return: number of leaves, 0 if tree is NULL
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	/* If node is a leaf, return 1 */
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	/* Sum leaves of left and right subtrees */
+	return (binary_tree_leaves(tree->left) +
+	        binary_tree_leaves(tree->right));
+}
